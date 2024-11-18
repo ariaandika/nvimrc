b050115ce3cd406e00c88cee6556451a506717c1
ariaandika <andikaakun17@gmail.com>
2024-11-18 18:11:28 +0700

    workaround for rust-analyzer bug

--- lua/ariaandika/lsp.lua
+++ lua/ariaandika/lsp.lua
@@ -76,6 +76,19 @@ local function lsp_setup()
         on_attach = on_attach,
         capabilities = capabilities
       }
+
+      -- workaround for horrible bug
+      -- [issue](https://github.com/neovim/neovim/issues/30985)
+      for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+          local default_diagnostic_handler = vim.lsp.handlers[method]
+          vim.lsp.handlers[method] = function(err, result, context, config)
+              if err ~= nil and err.code == -32802 then
+                  return
+              end
+              return default_diagnostic_handler(err, result, context, config)
+          end
+      end
+
     end,
     ts_ls = function()
       so('typescript-tools.nvim')
